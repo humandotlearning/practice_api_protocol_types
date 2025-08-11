89813c77a54f078a0c257d8e63d9798350d88560
humandotlearning <nithin.varghese1357@gmail.com>
2025-08-11 17:51:51 +0530

    feat: add websocket transcription endpoint and disable CORS middleware

--- websocket.py
+++ websocket.py
@@ -3,15 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app =FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
-    allow_methods =["GET"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://127.0.0.1:5500",
+#         "http://localhost:5500",
+#     ],
+#     allow_methods =["GET"],
+#     allow_headers=["*"]
+# )
 
 
 @app.websocket("/ws")
